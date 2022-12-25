33ffffc6a8f544684e40e82c1f39f4c66af84d69
AhmedRamiBoukef <ahmedramiboukef@gmail.com>
2022-12-25 20:00:38 +0100

    Adding Notifications

--- components/Dashboard/Dashboard.jsx
+++ components/Dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { MdNotifications } from 'react-icons/md';
 import { EmergencyList } from "./components/EmergencyList";
 import { PatientMap } from "./components/PatientMap";
+import io from "socket.io-client";
 
 export const Dashboard = () => {
-  const emergencyList = [
+  const emergency = [
     {
         Dangerous:"medium",
         recording:{
@@ -385,13 +387,60 @@ export const Dashboard = () => {
             confirm : true,
         }
     },
-];
+];  
+
+const [ isAlertVisible, setIsAlertVisible ] = useState(false);
+
+const handleButtonClick = () => {
+    setIsAlertVisible(true);
+    setTimeout(() => {
+      setIsAlertVisible(false);
+    }, 5000);
+}
+const [Notification,setNotification] = useState({
+  Dangerous:"high",
+  recording:{
+      name:"",
+  },
+  checked: false,
+  fast:false,
+  location: {
+      type: 'Point',
+      coordinates: [6.275271,35.638021]
+  },
+  patient: {
+      firstname: "rami",
+      lastname: "Ahmed",
+      birthdate: new Date("1995-12-17T03:24:00"),
+      email:"hospital@gmail.com",
+      phone:"077777777",
+      address:"batna",
+      confirm : true,
+  }
+})
+
+const socket = io.connect("http://localhost:5000/");
+  const [emergencyList,setEmergency] = useState(emergency)
+  useEffect(() => {
+    socket.on("receive_notification", (data) => {
+      setEmergency([data,...emergencyList]);
+      setNotification(data)
+      handleButtonClick();
+    });
+  }, [socket]);
   const [position,setPosition] = useState([6.275271,35.638021])
 
+
   return (
-        <div className="grid grid-cols-4">
+        <div className="grid grid-cols-4 relative">
           <div className="col-span-3"><PatientMap emergencyList={emergencyList} position={position}/></div>
           <div className="col-span-1"><EmergencyList emergencyList={emergencyList} setPosition={setPosition}/></div>
+          {isAlertVisible && <div className={`font-bold text-2xl absolute bottom-10 max-w-sm left-5 alert ${Notification.Dangerous == "low" ? "alert-success":Notification.Dangerous == "medium" ? "alert-warning": "alert-error"   } shadow-lg`}>
+            <div>
+              <MdNotifications className="w-8 h-8"/>
+              <span>Emergency Notification</span>
+            </div>
+          </div>}
         </div>
   )
 };
--- components/Dashboard/components/EmergencyList.jsx
+++ components/Dashboard/components/EmergencyList.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export const EmergencyList = ({setPosition,emergencyList}) => {
     
+    const protectionCivil = {
+            location: {
+                type:'Point',
+                coordinates: [
+                    6,7
+                ],
+            },
+            address: "ain yagout", 
+            email: "ks_sellami@esi.dz",
+            phone: "077777777",
+        }
+        
+    
     const dangerousStyle = {
         "low": "px-2 py-1 rounded-2xl bg-green-700 text-white",
         "medium": "px-2 py-1 rounded-2xl bg-yellow-500 text-white",
@@ -35,17 +48,41 @@ export const EmergencyList = ({setPosition,emergencyList}) => {
                         <p className='text-lg'>{calcAge(elem.patient.birthdate)}</p>
                         <p className='text-lg font-bold'>yo</p>
                     </div>
-                    <div className={elem.fast ? dangerousStyle["highfast"]:elem.Dangerous}>
+                    <div className={elem.fast ? dangerousStyle["highfast"]:dangerousStyle[elem.Dangerous]}>
                         {elem.Dangerous}
                     </div>
                 </label>
                 <input type="checkbox" id={`patientmodal${elem.id}`} className="modal-toggle" />
                     <label htmlFor={`patientmodal${elem.id}`} className="modal cursor-pointer">
-                    <label className="modal-box relative" htmlFor="">
-                        <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
-                        <p className="py-4">You ve been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                        <div className="modal-action">
-                            <label htmlFor={`patientmodal${elem.id}`} className="btn">Yay!</label>
+                    <label className="modal-box relative max-w-none w-[70%] h-[70%] " htmlFor="">
+                        <div className='flex flex-col justify-between items-center h-full'>
+                            <h1 className='w-full flex justify-center items-center text-5xl font-semibold'>Emergency Details</h1>
+                            <div className='text-lg flex justify-between items-center m-4 w-full bg-gray-200 py-6 px-6 rounded-xl'>
+                                <p>{elem.patient.firstname}</p>
+                                <p>{elem.patient.lastname}</p>
+                                <p>{calcAge(elem.patient.birthdate)}</p>
+                                <p>{elem.patient.email}</p>
+                                <p>{elem.patient.phone}</p>
+                                <p className={elem.fast ? dangerousStyle["highfast"]:dangerousStyle[elem.Dangerous]}>{elem.Dangerous}</p>
+                            </div>
+                            <div className='text-lg bg-gray-200 py-6 px-6 rounded-xl w-full m-4'>
+                                <h1 className='w-full flex justify-center items-center text-3xl mb-8 font-semibold'>Nearest Civil protection</h1>
+                                <div className='flex justify-between items-center '>
+                                    <p>Address</p>
+                                    <p>Email</p>
+                                    <p>Phone</p>
+                                    <div></div>
+                                </div>
+                                <div className='flex justify-between items-center '>
+                                    <p>{protectionCivil.address}</p>
+                                    <p>{protectionCivil.email}</p>
+                                    <p>{protectionCivil.phone}</p>
+                                    <button className='px-4 py-2 rounded bg-[#3CB79F] text-white'>Send Message</button>
+                                </div>
+                            </div>
+                            <div className="">
+                                <label htmlFor={`patientmodal${elem.id}`} className="w-full flex justify-center items-center text-3xl font-semibold px-12 py-4 rounded-lg bg-[#3CB79F] text-white">Check</label>
+                            </div>
                         </div>
                     </label>
                     
